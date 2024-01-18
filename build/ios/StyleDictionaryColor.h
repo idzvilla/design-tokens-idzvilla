ea867548540ce7cf57ded0c617233b0f546596c0
idzvilla <idzvilla@users.noreply.github.com>
2024-01-18 20:43:47 +0000

    Tokens updated

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,24 +4,26 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 18 Jan 2024 17:01:43 GMT
+// Generated on Thu, 18 Jan 2024 20:43:47 GMT
 
 
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-KdsTokensTextPrimary,
-KdsTokensTextInversed,
-KdsTokensBgPrimary,
-KdsTokensBgSecondary,
-KdsTokensBgBrandPrimary,
-KdsTokensFontSize14,
-KdsTokensFontSize16,
-KdsTokensFontSize18,
-KdsTokensFontSize20,
-KdsTokensLineHeight20,
-KdsTokensLineHeight24,
-KdsTokensLineHeight26
+KdsTextPrimary,
+KdsTextInversed,
+KdsBgPrimary,
+KdsBgSecondary,
+KdsBgBrandDefault,
+KdsBgBrandHover,
+KdsBgBrandActive,
+KdsFontSize14,
+KdsFontSize16,
+KdsFontSize18,
+KdsFontSize20,
+KdsLineHeight20,
+KdsLineHeight24,
+KdsLineHeight26
 };
 
 @interface StyleDictionaryColor : NSObject
