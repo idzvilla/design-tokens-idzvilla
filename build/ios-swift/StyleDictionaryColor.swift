1f645f4a15ec54a547089dfb1470bddbfdcca6eb
idzvilla <idzvilla@users.noreply.github.com>
2024-01-24 11:59:21 +0000

    Tokens updated

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,14 +4,14 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 24 Jan 2024 11:55:48 GMT
+// Generated on Wed, 24 Jan 2024 11:59:21 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
     public static let bgBrandActive = #008950ff
-    public static let bgBrandDefault = #00ad64ff
+    public static let bgBrandDefault = #ad003eff
     public static let bgBrandHover = #00ad64ff
     public static let bgPrimary = #ffffffff
     public static let bgSecondary = #f5f5f5ff
--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 24 Jan 2024 11:55:48 GMT
+// Generated on Wed, 24 Jan 2024 11:59:21 GMT
 
 
 #import "StyleDictionarySize.h"
